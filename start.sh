1ed695afa62b03a3e877514f5c6eff99285a959e
gangstar1010 <88017311+gangstar1010@users.noreply.github.com>
2021-11-20 19:53:58 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/gangstar1010/EvaMaria.git
+  git clone https://github.com/gangstar1010/EvaMaria.git /EvaMaria
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /EvaMaria
